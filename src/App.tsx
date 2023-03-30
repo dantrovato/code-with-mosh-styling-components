57d25fc597cae163796c9436c1525ef19d4b11cb
Daniele Trovato <plasticsoup@yahoo.co.uk>
2023-03-30 11:37:06 +0100

    Set up project and add ListGroup as well as importing bootstrap

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import ListGroup from "./components/ListGroup";
+import "bootstrap/dist/css/bootstrap.css";
+
+const App = () => {
+  return (
+    <div>
+      <ListGroup />
+    </div>
+  );
+};
+
+export default App;
