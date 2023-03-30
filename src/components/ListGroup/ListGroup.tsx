ec5fda308366c354acff866de92e036c4945d7b3
Daniele Trovato <plasticsoup@yahoo.co.uk>
2023-03-30 15:45:03 +0100

    Add css modules so that css rules are scoped locally

--- src/components/ListGroup/ListGroup.tsx
+++ src/components/ListGroup/ListGroup.tsx
@@ -1,7 +1,8 @@
-import "./ListGroup.css";
+import styles from "./ListGroup.module.css";
+
 const ListGroup = () => {
   return (
-    <ul className="list-group">
+    <ul className={[styles.ListGroup, styles.big].join(" ")}>
       <li className="list-group-item">An item</li>
       <li className="list-group-item">A second item</li>
       <li className="list-group-item">A third item</li>
