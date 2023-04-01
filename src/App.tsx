d6beffba7655dea79d6023397e319b309e3b9011
Daniele Trovato <plasticsoup@yahoo.co.uk>
2023-04-01 12:57:26 +0100

    Set up App for the exercise. Num 11 - Updating state

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,15 @@
 import { useState } from "react";
-import Cart from "./components/Cart";
-import NavBar from "./components/NavBar";
-const App = () => {
-  const [cartItems, setCartItems] = useState(["Product 1", "Product 2"]);
-  const handleClear = () => {
-    const newItems = [...cartItems];
-    newItems.shift();
-    setCartItems(newItems);
-  };
 
-  return (
-    <div>
-      <NavBar cartItemsCount={cartItems.length} />
-      <Cart cartItems={cartItems} onClear={handleClear} />
-    </div>
-  );
+const App = () => {
+  const [game, setGame] = useState({
+    id: 1,
+    player: {
+      name: "John",
+    },
+  });
+
+  const handleClick = () => {};
+  return <div> </div>;
 };
 
 export default App;
