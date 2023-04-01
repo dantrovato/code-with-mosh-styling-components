3b741386bcccbf845e88604c9519b5217fcf7031
Daniele Trovato <plasticsoup@yahoo.co.uk>
2023-04-01 19:52:47 +0100

    Set up next excercise still on lesson 12

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
 import { useState } from "react";
 
 const App = () => {
-  const [game, setGame] = useState({
-    id: 1,
-    player: {
-      name: "John",
-    },
+  const [pizza, setPizza] = useState({
+    name: "Spicy Pepperoni",
+    toppings: ["Mushroom"],
   });
 
-  const handleClick = () => {
-    setGame({ ...game, player: { ...game.player, name: "Dave" } });
-  };
+  const handleClick = () => {};
   return (
     <div>
-      <p>{game.player.name}</p>
       <button onClick={handleClick}>Click</button>
     </div>
   );
